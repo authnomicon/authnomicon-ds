b7c54ee246596fd911067359983def5c70843bd8
Jared Hanson <jaredhanson@gmail.com>
2018-03-20 14:08:37 -0700

    Copy realm components from aaa.

---
+++ app/realms/main.js
@@ -0,0 +1,27 @@
+exports = module.exports = function(resolver, passwordVerifierFactory, directoryFactory) {
+  var Realm = require('../../lib/realms/realm');
+  
+  
+  var api = {};
+  
+  api.resolve = function(name, cb) {
+    resolver.resolve(name, function(err, config) {
+      if (err) { return cb(err); }
+      
+      var realm = new Realm(name, config);
+      realm._directoryFactory = directoryFactory;
+      realm._passwordVerifierFactory = passwordVerifierFactory;
+      return cb(null, realm);
+    });
+  };
+  
+  return api;
+};
+
+exports['@implements'] = 'http://schemas.modulate.io/js/aaa/realms';
+exports['@singleton'] = true;
+exports['@require'] = [
+  './resolver',
+  '../ds/authentication/password/verifierfactory',
+  '../ds/directory/factory'
+];
